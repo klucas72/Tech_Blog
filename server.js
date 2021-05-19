11e84642d0ba7b48e61059a1c1ccdd8d185f5cdb
klucas72 <klucas72@hotmail.com>
2021-05-19 19:30:24 -0400

    working on finalized package.json and server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-
+const routes = require('./controllers');
+const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
