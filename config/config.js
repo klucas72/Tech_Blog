cf49c4b5b872607d5057426cb13d7ece1b59bf64
klucas72 <klucas72@hotmail.com>
2021-05-23 20:15:01 -0400

    restored config file back to original state

--- config/config.js
+++ config/config.js
@@ -5,8 +5,10 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.MYSQL_URL, {
-    dialect: "mysql",
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
   });
 
 module.exports = sequelize;
